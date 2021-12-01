5271302120a6c60bb5651f3eecb83b2fe985bd0b
mrabets <newrabgo@gmail.com>
2021-12-01 21:06:25 +0300

    Add models tests

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :likes, dependent: :destroy
+
+  validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :photos
 
-  validates :username, :password, presence: true
-  validates :username, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :password, presence: true, length: { minimum: 6 }
 end
