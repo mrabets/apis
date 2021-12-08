8ea22b1a3693befa965ac95c60055dcaa9cdc77b
Karim Marabet <newrabgo@gmail.com>
2021-12-08 18:53:10 +0300

    Set token to sign on request

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,15 @@
 module Users
   class SessionsController < Devise::SessionsController
     respond_to :json
+
+    private
+
+    def respond_with(_resource, _opts = {})
+      render json: { user: current_user, token: current_token }, status: :ok
+    end
+
+    def current_token
+      request.env['warden-jwt_auth.token']
+    end
   end
 end
