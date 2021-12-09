446f39219c2a15a9e8f56b774f483929d150deb2
Karim Marabet <newrabgo@gmail.com>
2021-12-09 16:53:40 +0300

    Render users controller errors

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,19 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
     respond_to :json
+
+    private
+
+    def respond_with(_resource, _opts = {})
+      if current_user
+        render json: { user: current_user, token: current_token }, status: :ok
+      else
+        render json: { error: @user.errors.full_messages }, status: 422
+      end
+    end
+
+    def current_token
+      request.env['warden-jwt_auth.token']
+    end
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,11 @@ module Users
     private
 
     def respond_with(_resource, _opts = {})
-      render json: { user: current_user, token: current_token }, status: :ok
+      if current_user
+        render json: { user: current_user, token: current_token }, status: :ok
+      else
+        render json: { error: @user.errors.full_messages }, status: 422
+      end
     end
 
     def current_token
